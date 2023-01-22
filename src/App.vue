08ab5d09b899b73244accc67c051704ec8ec4ef0
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-01-22 22:42:21 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { logLabeled } from "./utils/logger";
+logLabeled("I'm ready!  ⸜(๑'ᵕ'๑)⸝⋆*", "log", "", "color: #66ccff");
+</script>
+
+<template>
+  <RouterView />
+</template>
